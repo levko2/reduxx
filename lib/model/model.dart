df842d9a0796d36dcd4598c8f47a0b9c124405c4
alexander <Password>
2019-05-18 17:06:46 +0300

    Init commit.

---
+++ lib/model/model.dart
@@ -0,0 +1,25 @@
+
+
+ class Item{
+  final int id;
+  final String body;
+
+  Item({this.id, this.body});
+
+  Item copyWith({int id, String body}){
+    return Item(
+      id: id ?? this.id,
+      body: body ?? this.body
+    );
+  }
+
+ }
+ class AppState{
+  final List<Item> items;
+
+  AppState({this.items});
+
+  AppState.initialState() : items= List.unmodifiable(<Item>[]);
+
+
+ }
